89e08481dff7248e6ce3ac678e5dcb25313f2735
MayThinHan <maythinhan@mm.cybermissions.co.jp>
2019-07-29 20:24:48 +0900

    update d,p

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.log_formatter = LogFormatter.new
+  config.log_formatter = MessageFormatter.new
   config.log_level = :debug 
 
     
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.log_formatter = LogFormatter.new
+  config.log_formatter = MessageFormatter.new
   config.log_level = :debug 
 
 end
