f0f9d9b0f8983ff78d899547aad3e568e5ff103e
MayThinHan <maythinhan@mm.cybermissions.co.jp>
2019-07-30 12:53:44 +0900

    upload final code

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.application.configure do 
-
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-end
