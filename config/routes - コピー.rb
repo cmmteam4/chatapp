e6d1167a2b08f24e3e215a95a3d1e09865d9c38e
MayThinHan <maythinhan@mm.cybermissions.co.jp>
2019-07-30 12:48:46 +0900

    upload final code

---
+++ config/routes - コピー.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|ja/ do
+    root   'welcome_page#home'      
+
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    delete '/logout',  to: 'sessions#destroy'   
+    get    '/choose_workspace',    to: 'workspaces#workspace' 
+    get    '/workspaceList',    to: 'workspaces#wksList'  
+    get    '/inviteFriend',    to: 'wksinvite#index'
+    get    '/edit',   to: 'users#edit'
+    get    '/favourite',   to: 'messages#favourite'   
+
+   get "/star",to:"messages#ssss"
+   post "/star",to:"messages#star"
+
+    resources :users
+    resources :messages
+  
+    resources :workspaces do
+      resources :wksinvite
+    end
+
+    resources :channels do
+      resources :invite
+    end 
+ end 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,26 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|ja/ do
-    root   'welcome_page#home'      
+  root   'welcome_page#home'      
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'   
+  get    '/choose_workspace',    to: 'workspaces#workspace' 
+  get    '/workspaceList',    to: 'workspaces#wksList'  
+  get    '/inviteFriend',    to: 'wksinvite#index'
+  get    '/edit',   to: 'users#edit'
+  get    '/favourite',   to: 'messages#favourite'   
 
-    get    '/login',   to: 'sessions#new'
-    post   '/login',   to: 'sessions#create'
-    delete '/logout',  to: 'sessions#destroy'   
-    get    '/choose_workspace',    to: 'workspaces#workspace' 
-    get    '/workspaceList',    to: 'workspaces#wksList'  
-    get    '/inviteFriend',    to: 'wksinvite#index'
-    get    '/edit',   to: 'users#edit'
-    get    '/favourite',   to: 'messages#favourite'   
+  get "/star",to:"messages#ssss"
+  post "/star",to:"messages#star"
 
-   get "/star",to:"messages#ssss"
-   post "/star",to:"messages#star"
+  resources :users
+  resources :messages
 
-    resources :users
-    resources :messages
-  
-    resources :workspaces do
-      resources :wksinvite
-    end
+  resources :workspaces do
+    resources :wksinvite
+  end
 
-    resources :channels do
-      resources :invite
-    end 
- end 
+  resources :channels do
+    resources :invite
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
